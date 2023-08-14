0933579413c3ce76ed3d3a80f0a7d325b059ceee
MalekHasan <malek.alsheekh01@gmail.com>
2023-08-14 09:48:07 +0000

    convert class component to function component

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,41 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React ,{useState} from 'react';
 
-function App() {
-  const [count, setCount] = useState(0)
+import './App.scss';
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+// Let's talk about using index.js and some other name in the component folder
+// There's pros and cons for each way of doing this ...
+import Header from './components/header/Header';
+import Footer from './components/footer/Footer';
+import Form from './components/form/Form';
+import Results from './components/results/Results';
 
-export default App
+const App= (props)=>{
+const [data,setData]=useState(null)
+const [requestParams,setRequestParams]=useState({})
+
+ const callApi = (requestParams) => {
+    // mock output
+    const data = {
+      count: 2,
+      results: [
+        {name: 'fake thing 1', url: 'http://fakethings.com/1'},
+        {name: 'fake thing 2', url: 'http://fakethings.com/2'},
+      ],
+    };
+    setData(data)
+    setRequestParams(requestParams)
+  }
+
+    return (
+      <>
+        <Header />
+        <div>Request Method: {requestParams.method}</div>
+        <div>URL: {requestParams.url}</div>
+        <Form handleApiCall={callApi} />
+        <Results data={data} />
+        <Footer />
+      </>
+    );
+  }
+
+export default App;
